c7c2d756a547006c69388695c97ba804149142e8
Steve Lukis <stevelukis88@gmail.com>
2021-12-09 14:08:01 +0800

    Add a spinner while waiting for tokenList

--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useContract } from "../hooks/useContract";
-import { Container, Row, Col, Alert } from "react-bootstrap";
+import { Container, Row, Col, Alert, Spinner } from "react-bootstrap";
 
 type Props = {
     currentAccount?: string
@@ -11,7 +11,11 @@ const Collection: React.FC<Props> = ({ currentAccount }) => {
     return (
         <Container className="mt-5">
             <Row className="justify-content-center">
-                <Col lg={6}>
+                <Col lg={6} className="d-flex justify-content-center">
+                    {!tokenList &&
+                    <Spinner animation="grow" variant="warning" />
+                    }
+
                     {tokenList && tokenList.length === 0 &&
                     <Alert variant="warning">You don't have any token yet!</Alert>
                     }
