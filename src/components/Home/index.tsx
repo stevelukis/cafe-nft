367d7c1c22b9bbcf3405007a883d8de7da2bf1ad
Steve Lukis <stevelukis88@gmail.com>
2021-12-10 09:43:33 +0800

    Add a description text in the home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -11,18 +11,14 @@ type Props = {
 }
 
 const Home: React.FC<Props> = ({ currentAccount }) => {
-    const [, setTotalSupply] = useState<number | null>(null);
-    const [, setMaxSupply] = useState<number | null>(null);
     const [tokenLeft, setTokenLeft] = useState<number | null>(null);
     const [error, setError] = useState(false);
 
     useEffect(() => {
         const fetchSupply = async () => {
-            const ts = await getTotalSupply();
-            const ms = await getMaxSupply();
-            setTotalSupply(ts);
-            setMaxSupply(ms);
-            setTokenLeft(ms - ts);
+            const totalSupply = await getTotalSupply();
+            const maxSupply = await getMaxSupply();
+            setTokenLeft(maxSupply - totalSupply);
         }
         fetchSupply();
     }, []);
@@ -49,14 +45,34 @@ const Home: React.FC<Props> = ({ currentAccount }) => {
                         <Row>
                             <Image
                                 src={Logo}
-                                width={0.8}
                             />
                         </Row>
                     </Col>
+                    <Col lg={6} md={12} className="text-center">
+                        <Row>
+                            <h1 className="text-black">Cafe NFT</h1>
+                        </Row>
+                        <Row className="mt-3">
+                            <p className="text-body">
+                                Welcome cafe lovers! Cafe NFT is a rare collection of 5 uniquely art cafe slice of life.
+                                Owning a cafe NFT is not just holding an NFT but it’s also owning a membership to our
+                                Cafe franchise all over the world. That means you are eligible for 20% discounts to our
+                                foods and drinks. You are also have the right to get special role in our Discord server
+                                and hang out with the community. It is also a great long term investment to hold our
+                                NFTs because they are backed up by a big FnB business. Cafe NFTs aren't speculative
+                                because they are indeed very rare and have real life benefits.
+                            </p>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="mt-5 justify-content-center">
                     <Col lg={6} md={12}>
                         <Row>
                             <Col className="d-flex justify-content-md-center">
-                                {tokenLeft &&
+                                {tokenLeft && tokenLeft === 0 &&
+                                <h1>Sold out!</h1>
+                                }
+                                {tokenLeft && tokenLeft > 0 &&
                                 <h1>{tokenLeft} token{tokenLeft > 1 && 's'} left!</h1>
                                 }
                             </Col>
